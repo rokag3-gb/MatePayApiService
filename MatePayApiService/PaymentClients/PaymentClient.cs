9e0e6dde8d7f061f83e63b9bb6cc93ffe78fdc25
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-03-26 17:41:30 +0900

    PaymentClient 클래스 추가

---
+++ MatePayApiService/PaymentClients/PaymentClient.cs
@@ -0,0 +1,12 @@
+namespace MatePayApiService.PaymentClients
+{
+    public class PaymentClient
+    {
+        public boolean useProduction { get; set; }
+        private string paymentEndpointUrl => isProduction? "gw.easypay.co.kr" : "testgw.easypay.co.kr";
+        private string paymentEndpointPort => "80";
+        public string certFilePath { get; set; }
+        public string logFilePath { get; set; }
+        public int logLever { get; set; }
+    }
+}
