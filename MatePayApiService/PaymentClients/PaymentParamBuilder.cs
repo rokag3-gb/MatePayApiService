e3e73a113c282b799398451daac56711a0d00a55
Youngbin Han <sukso96100@gmail.com>
2021-03-29 14:50:53 +0900

    Payment 파라메터 빌더 및 열거형 데이터 클래스 추가

---
+++ MatePayApiService/PaymentClients/PaymentParamBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MatePayApiService.PaymentClients
+{
+    public class PaymentParamBuilder
+    {
+        private StringBuilder paramBuilder;
+        public PaymentParamBuilder()
+        {
+            this.paramBuilder = new StringBuilder("");
+        }
+        public void StartSection(string sectionName)
+        {
+            this.paramBuilder.Append(sectionName);
+            this.paramBuilder.Append("=");
+        }
+        public void Add(string key, string value)
+        {
+            this.paramBuilder.Append(key);
+            this.paramBuilder.Append("=");
+            this.paramBuilder.Append(value);
+            this.paramBuilder.Append(Convert.ToChar(31).ToString());
+        }
+        public void EndSection()
+        {
+            // 레코드 경계 할당 문자 추가
+            this.paramBuilder.Append(Convert.ToChar(30).ToString());
+        }
+        public void SplitSection()
+        {
+            // 파일 경계 할당 문자 추가
+            this.paramBuilder.Append(Convert.ToChar(28).ToString());
+        }
+        override public string ToString()
+        {
+            return this.paramBuilder.ToString();
+        }
+    }
+}
---
+++ MatePayApiService/PaymentClients/PaymentParamEnums.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatePayApiService.PaymentClients
+{
+    // 결제 처리 코드
+    public static class TransactionCode
+    {
+        // 승인
+        public static string APPROVE_PAYMENT => "00101000";
+        // 변경
+        public static string MODIFY_PAYMENT => "00201000";
+    }
+    // 결제 사용자 유형
+    public static class ConsumerType
+    {
+        // 개인
+        public static string INDIVIDUAL => "0";
+        // 법인
+        public static string CORPORATION => "1";
+
+        public static string getFromIdentifyCode(string identifyCode) 
+        {
+            return identifyCode.Trim().Replace("-", "").Length == 10 ? CORPORATION : INDIVIDUAL;
+        }
+    }
+    // 카드 무이자 할부 여부
+    public static class CardCreditInterestsType
+    {
+        // 일반
+        public static string DEFAULT => "00";
+        // 무이자
+        public static string NO_INTERESTS => "02";
+    }
+    // 결제 인증 유형
+    public static class PaymentCertType
+    {
+        // 인증(암호 없이)
+        public static string CERT_WITHOUT_PASSWORD = "0";
+        // 비인증(기본값)
+        public static string DEFAULT_NO_CERT = "1";
+        // 구인증(미사용)
+        public static string LEGACY_CERT = "2";
+    }
+}
